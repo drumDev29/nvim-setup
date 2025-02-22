7b8a8b98c947c526aa96370e31885a24de588c90
Kyle Taylor <kyle1324@hotmail.com>
2025-02-22 15:30:26 -0600

    wsl ubuntu setup file with bash (doesn't work yet)

---
+++ ubuntu-setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo >> /home/kyle/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/kyle/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt-get install build-essential
+
+# Install build dependencies
+sudo apt-get update
+sudo apt-get install git
+
+# Install packages via Homebrew
+brew install neovim
+brew install ripgrep
+brew install fd
+brew install python
+brew install node
+brew install lazygit
+
+# Create necessary directories
+mkdir -p ~/.config/nvim
+mkdir -p ~/.config/lazygit
+
+# Copy configuration files
+cp -r /mnt/c/code/nvim-setup/ ~/.config/nvim/
+cp /mnt/c/code/docker/.gitconfig ~/.gitconfig
+cp /mnt/c/code/docker/lazygitconfig.yml ~/.config/lazygit/config.yml
+
