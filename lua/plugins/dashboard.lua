0535e0e95e174f4122866d4410c9c6147337c52f
Kyle Taylor <kyle1324@hotmail.com>
2025-04-17 02:20:53 -0500

    deck out dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -9,8 +9,12 @@ return {
           { key = "f", action = "<Leader>ff", icon = get_icon("Search", 0, true), desc = "Find File  " },
           { key = "w", action = "<Leader>fw", icon = get_icon("WordFile", 0, true), desc = "Find Word  " },
           { key = "e", action = "<Leader>e", icon = get_icon("FolderOpen", 0, true), desc = "Explorer  " },
-          { key = "a", action = "<Leader>ac", icon = get_icon("Environment", 0, true), desc = "AI Chat  " },
+          { key = "g", action = "<Leader>gg", icon = get_icon("Git", 0, true), desc = "Git  " },
+          { key = "p", action = "<Leader>ps", icon = get_icon("Package", 0, true), desc = "Plugins " },
+          { key = "L", action = "<Leader>pm", icon = get_icon("ActiveLSP", 0, true), desc = "LSP  " },
+          { key = "a", action = "<Leader>ac", icon = get_icon("Environment", 0, true), desc = "AI" },
           { key = "s", action = "<Leader>S.", icon = get_icon("Refresh", 0, true), desc = "Last Session  " },
+          { key = "q", action = "<Leader>Q", icon = get_icon("TabClose", 0, true), desc = "Quit  " },
         },
         header = table.concat({
           "██╗  ██╗██╗   ██╗██╗     ███████╗███╗   ██╗██╗   ██╗██╗███╗   ███╗",
@@ -24,7 +28,14 @@ return {
       sections = {
         { section = "header", padding = 5 },
         { section = "keys", gap = 1, padding = 3 },
-        { section = "startup" },
+        {
+          section = "terminal",
+          enabled = function() return Snacks.git.get_root() ~= nil end,
+          cmd = "git status --short --branch --renames",
+          height = 5,
+          padding = 1,
+        },
+        -- { section = "startup" },
       },
     }
   end,
