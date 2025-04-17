a2434be637f838d05fedb04444882f2d207b3529
Kyle Taylor <kyle1324@hotmail.com>
2025-04-17 00:49:23 -0500

    disable resession

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -28,31 +28,6 @@ return {
         ["<C-t>"] = { "<Esc><Cmd>ToggleTerm<CR>", desc = "Toggle terminal" },
       },
     },
-    sessions = {
-      -- Configure auto saving
-      autosave = {
-        last = true, -- auto save last session
-        cwd = true, -- auto save session for each working directory
-      },
-    },
-    autocmds = {
-      -- disable alpha autostart
-      alpha_autostart = false,
-      restore_session = {
-        {
-          event = "VimEnter",
-          desc = "Restore previous directory session if neovim opened with no arguments",
-          nested = true, -- trigger other autocommands as buffers open
-          callback = function()
-            -- Only load the session if nvim was started with no args
-            if vim.fn.argc(-1) == 0 then
-              -- try to load a directory session using the current working directory
-              require("resession").load(vim.fn.getcwd(), { dir = "dirsession", silence_errors = true })
-            end
-          end,
-        },
-      },
-    },
   },
   require("toggleterm").setup {
     shell = "powershell.exe",
